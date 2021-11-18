3489a70d277888265feec0858d4ef651a1cf601c
Kanak Gautam <81749791+kanak2106@users.noreply.github.com>
2021-11-18 09:41:56 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,10 +1,10 @@
-let loc = document.getElementById("location");
-let tempicon = document.getElementById("temp-icon");
-let tempvalue = document.getElementById("temp-value");
-let climate = document.getElementById("climate");
+let loc = document.getElementsByClassName("location");
+let tempicon = document.getElementsByClassName("temp-icon");
+let tempvalue = document.getElementsByClassName("temp-value");
+let climate = document.getElementsByClassName("climate");
 let iconfile;
-const searchInput = document.getElementById("search-input");
-const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementsByClassName("search-input");
+const searchButton = document.getElementsByClassName("search-button");
 
 
 
@@ -24,14 +24,32 @@ window.addEventListener("load" , ()=>
         { 
         long= position.coords.longitude;
         lat= position.coords.latitude;
+        const proxy="https://cors-anywhere.herokuapp.com/"
 
-            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={1860f550f372cd463fb36f25533f51b0}`
+            const api = `$ {proxy} api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={1860f550f372cd463fb36f25533f51b0}`
 
             fetch(api). then((Response)=>
             {
                 return Response.json();
 
             })
+
+            .then ( data => 
+                {
+
+                    const {name} = data ;
+                    const {feels_like} = data.main ;
+                    const {id, main} = data.weather[0] ;
+
+                    loc.textContent =name;
+                    climate.textContent=main;
+                    tempvalue.textContent=Math.round(feels_like-273)
+
+
+
+
+
+               })
 
 
         }
